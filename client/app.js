187e968ae2b255221c01624bae870196536898ba
Pokegear <christoph.ding@gmail.com>
2016-09-10 17:21:57 -0400

    Added ui-routing to angular.  Made a home partial view.

--- client/app.js
+++ client/app.js
@@ -1,1 +1,10 @@
-var app = angular.module('mainApp', []);
+var app = angular.module('mainApp', ['ui.router'])
+
+app.config(function($stateProvider) {
+  $stateProvider
+
+  .state('home', {
+    url: '/home',
+    templateUrl: '/components/home/home.html'
+  })
+})
