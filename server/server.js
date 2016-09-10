0d33cec06aaff8674d5ed93fc1c4ceb096b4ce18
Ena Bek <enabek@gmail.com>
2016-09-10 14:33:53 -0400

    Add working server and gitignore file and express

---
+++ server/server.js
@@ -0,0 +1,11 @@
+//dependencies
+var express = require('express');
+
+//server and connection
+var app = express();
+var port = 8000;
+
+app.listen(port, function(){
+  console.log('Working');
+});
+
