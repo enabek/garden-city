fd4d95fa0953dd1e145a0a07c17ec8e438f758b4
Ena Bek <enabek@gmail.com>
2016-09-10 15:00:51 -0400

    Add index file and server serves index

--- server/server.js
+++ server/server.js
@@ -1,11 +1,20 @@
 //dependencies
 var express = require('express');
+var path = require('path');
 
 //server and connection
 var app = express();
 var port = 8000;
 
 app.listen(port, function(){
-  console.log('Working');
+  console.log('Listening');
 });
 
+app.use(function(req, res, next){
+  console.log('Hitting the server at all');
+  next();
+});
+
+app.get('/', function(req, res){
+  res.sendFile(path.join(__dirname + '/../index.html'));
+});
