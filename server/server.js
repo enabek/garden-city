00bd863c7221eb5fc894e85fc09458d2cd07434b
Pokegear <christoph.ding@gmail.com>
2016-09-11 13:29:43 -0400

    Added more server logging.

--- server/server.js
+++ server/server.js
@@ -1,27 +1,28 @@
 //dependencies
 var express = require('express');
 var path = require('path');
+var bodyParser = require('body-parser');
+var morgan = require('morgan');
+
+var jwt    = require('jsonwebtoken'); // used to create, sign, and verify tokens
 
 //server and connection
 var app = express();
 var port = 8000;
-
 app.listen(port, function(){
   console.log('Listening');
 });
+
+// middleware
+app.use(bodyParser.urlencoded( {extended: false} ));
+app.use(bodyParser.json());
+app.use(morgan('dev'));
+
+// api 
+app.use(express.static(path.join(__dirname + '/../client')));
 
 app.use(function(req, res, next){
   console.log('Hitting the server at all');
   next();
 });
 
-app.use(express.static(path.join(__dirname + '/../client')));
-
-// app.get('/', function(req, res){
-//   res.sendFile(path.join(__dirname + '/../index.html'));
-// });
-
-app.get('/test', function(req, res){
-  console.log(req);
-  res.send('Test is working');
-});
